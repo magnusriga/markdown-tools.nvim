52cd4272edebb5fe58958927bdebb7fbb264730d
Magnus <magnus.rinnan@gmail.com>
2025-05-05 22:21:12 +0200

    chore: setup husky and commitlint for commit conventions

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+// commitlint.config.js
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  // You can add custom rules here if needed
+  // rules: {
+  //   'scope-enum': [2, 'always', ['core', 'commands', 'config', 'docs', 'ci']],
+  // }
+};
